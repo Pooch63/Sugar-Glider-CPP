5b8e1f4e98440e99e21894b94055619ceef480ff
Pooch63 <99148663+Pooch63@users.noreply.github.com>
2024-12-21 19:44:35 -0500

    Delete node on error

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "ast.hpp"
-#include "IR/bytecode.hpp"
-#include "IR/intermediate.hpp"
+#include "ir/bytecode.hpp"
+#include "ir/intermediate.hpp"
 #include "compiler.hpp"
 #include "errors.hpp"
 #include "lexer.hpp"
@@ -11,22 +11,28 @@
 int main() {
     Parse::Parser::initialize_parse_rules();
 
-    std::string prog = "1 ? 2 + 4 : 3";
+    std::string prog = "var g = 3; g;";
     Scan::Scanner lexer(prog);
 
     Output output(prog);
 
     Parse::Parser parser(lexer, output);
-    AST::Node* node = parser.parse_precedence(-1);
+    AST::Node* node = parser.parse_statement();
 
-    auto block = Intermediate::Block();
+    std::cout << "DID PARSING WORK?" << std::endl;
+
     // block.add_instruction(Intermediate::Instruction(Intermediate::INSTR_GOTO, 1));
     // block.add_instruction(Intermediate::Instruction(Intermediate::INSTR_GOTO, 2));
     // block.log_block();
 
     /* If there was a parse error, there will be some null pointers, so DO NOT
         actually compile. */
-    if (output.had_error()) return -1;
+    if (output.had_error()) {
+        delete node;
+        return -1;
+    }
+
+    auto block = Intermediate::Block();
 
     using namespace Bytecode;
     Chunk chunk = Chunk();
@@ -35,9 +41,9 @@ int main() {
     Compiler compiler(block);
     compiler.compile(node);
 
+
     // chunk.print_code();
     block.log_block();
-
 
     delete node;
 
