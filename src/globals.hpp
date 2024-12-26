7af0d9b7dfdc5c92fbf03faa34d142c6d7c6f49f
Pooch63 <99148663+Pooch63@users.noreply.github.com>
2024-12-26 12:40:02 -0500

    New flag

--- src/globals.hpp
+++ src/globals.hpp
@@ -2,6 +2,7 @@
 #define _SGCPP_GLOBALS_HPP
 
 #define DEBUG
+#define DEBUG_ASSERT // run every single assertion to make sure the program is working. Certain assertions may still be run if DEBUG is set
 
 #ifdef DEBUG
     #include <iostream>
